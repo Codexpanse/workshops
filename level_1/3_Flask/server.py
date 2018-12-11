e3b40ddc4c97e74fb955e77cd02e84f2cb4b44bb
Rakhim Davletkaliyev <freetonik@gmail.com>
2018-12-11 12:44:55 +0200

    update

--- level_1/3_Flask/server.py
+++ level_1/3_Flask/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return app.send_static_file('./index.html')
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='3000')
