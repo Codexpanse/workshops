7a2576fbb017f50d5a428c2a70ceb27bc1158a7b
Rakhim <freetonik@gmail.com>
2018-12-20 13:51:20 +0200

    add print

--- level_1/2_simple_HTTP_server/server.py
+++ level_1/2_simple_HTTP_server/server.py
@@ -2,4 +2,5 @@ import http.server
 import socketserver
 
 with socketserver.TCPServer(("", 8080), http.server.SimpleHTTPRequestHandler) as httpd:
+    print("Server started...")
     httpd.serve_forever()
